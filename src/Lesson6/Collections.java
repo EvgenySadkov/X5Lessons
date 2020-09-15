a70d41e459a6f8a0da21838e8e6e12236105ac09
Escape <mmski@mail.ru>
2020-09-15 21:46:24 +0400

    Начал 6 задание

---
+++ src/Lesson6/Collections.java
@@ -0,0 +1,12 @@
+package Lesson6;
+
+import java.util.ArrayList;
+
+public class Collections {
+
+
+
+
+
+}
+
---
+++ src/Lesson6/Main.java
@@ -0,0 +1,11 @@
+package Lesson6;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> collection1 = new ArrayList<>();
+        collection1.add("aaa");
+        System.out.println(collection1.size());
+    }
+}
