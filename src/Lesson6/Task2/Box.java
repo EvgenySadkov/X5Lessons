7d0de293d0443d3519c94b0dab560655ed56ad90
Escape <mmski@mail.ru>
2020-09-18 21:49:28 +0400

    Дорешал 2 задачу 6 урока

--- src/Lesson6/Task2/Box.java
+++ src/Lesson6/Task2/Box.java
@@ -31,4 +31,57 @@ public class Box<T, E> {
         System.out.println(Arrays.toString(box));
     }
 
+    public double getWeight() {
+        double temp = 0;
+        if (box.length == 0) {
+            temp = 0;
+        } else {
+            if (box[0] instanceof Orange) {
+                temp = (box.length * 1.5);
+            } else {
+                if (box[0] instanceof Apple) {
+                    temp = (box.length * 1);
+                }
+            }
+
+        }
+        return temp;
+    }
+
+    public boolean compare(Box underBox) {
+
+        return (this.getWeight() == underBox.getWeight());
+    }
+
+    public Object getFruit(int i) {
+        return box[i];
+    }
+
+    public int getLength() {
+        return box.length;
+    }
+
+    public void sprinkle(Box to) {//задание со *
+        if ((box[0] instanceof Orange) & (to.getFruit(0) instanceof Orange) | (box[0] instanceof Apple) & (to.getFruit(0) instanceof Apple)) {
+            Object[] result = new Object[box.length + to.getLength()];
+               for (int i = 0; i < to.getLength(); i++) {
+                result[i] = to.getFruit(i);
+                      }
+               for (int i= to.getLength(); i<box.length + to.getLength(); i++){
+                   result[i]=box[i-to.getLength()];
+               }
+            box = result;
+               to.clear();
+
+        }
+        else {
+            System.out.println("Нельзя сыпать разные фрукты в одну коробку");
+        }
+    }
+
+    private void clear() {
+        for (int i=0; i<box.length; i++){
+            box[i]=null;
+        }
+    }
 }
--- src/Lesson6/Task2/Main.java
+++ src/Lesson6/Task2/Main.java
@@ -6,6 +6,7 @@ public class Main {
         Apple<Integer> app2 = new Apple<>(2);
         Orange<Integer> or1 = new Orange<>(1);
         Orange<Integer> or2 = new Orange<>(2);
+        Orange<Integer> or3 = new Orange<>(3);
         Box box1 = new Box<>();
         box1.add(app1);
         box1.add(app2);
@@ -13,7 +14,18 @@ public class Main {
         Box box2 = new Box<>();
         box2.add(or1);
         box2.add(or2);
+        Box box3 = new Box();
+        box3.add(or3);
+        box3.add(or1);
         box1.print();
         box2.print();
+        box3.print();
+        System.out.println("Вес коробки:" + box1.getClass()+ " " + box1.getWeight());
+        System.out.println("Вес коробки:" + box2.getClass()+ " " + box2.getWeight());
+        System.out.println(box1.compare(box2));
+        box2.sprinkle(box3);//в первую коробку не идет
+        box1.print();
+        box2.print();
+        box3.print();
     }
 }
